a68200621b076d231f6ca8876b98700a5c809a06
Ivan <Ivandgea@gmail.com>
2023-01-30 12:00:30 +0100

    push

---
+++ Ironhack/Apuntes de la Classe/funciones.js
@@ -0,0 +1,8 @@
+
+//Funcion : bloque de codigo
+
+console.log("hola desde funciones.js")
+console.log("hola desde funciones.js")
+console.log("hola desde funciones.js")
+console.log("hola desde funciones.js")
+console.log("hola desde funciones.js")
---
+++ Ironhack/ApuntesdelaClasse/funciones.js
@@ -0,0 +1,12 @@
+console.log("FUNCIONA")
+
+//Recorrer una ARRAY
+let alumnos = ["jasjf", "gaga", "gagag", "agags", "gasg", "agasg"];
+//FOR
+
+
+for (let i = 0; i < alumnos.length; i++) {
+    console.log(i + "-" + alumnos[i]);
+}
+
+
